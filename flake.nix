6163319f3c9cea8367d725431685948831b165c4
saygo-png <saygo.mail@proton.me>
2025-07-23 01:20:31 +0200

    init
    Signed-off-by: saygo-png <saygo.mail@proton.me>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixvim.url = "github:nix-community/nixvim/nixos-25.05";
+  outputs = {nixvim, ...} @ inputs: let
+    inherit (nixvim.inputs.nixpkgs) lib;
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    eachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixvim.inputs.nixpkgs {
+        inherit system;
+      });
+  in {
+    packages = eachSystem (pkgs: let
+      nixvimModule = {
+        inherit pkgs;
+        module = import ./config.nix;
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+      };
+    in rec {
+      neovim = nixvim.legacyPackages.${pkgs.system}.makeNixvimWithModule nixvimModule;
+      default = neovim;
+    });
+  };
+}
