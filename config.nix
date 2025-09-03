2eed784034f94f8989af83e7567bbcc0f117048b
saygo-png <saygo.mail@proton.me>
2025-09-03 16:20:35 +0200

    faster bug
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config.nix
+++ config.nix
@@ -1,2 +1,20 @@
 _: {
+  plugins = {
+    web-devicons.enable = true; # disable warn
+
+    telescope.enable = true;
+    faster.enable = true;
+  };
+
+  performance = {
+    byteCompileLua = {
+      enable = true;
+      plugins = true;
+
+      configs = false;
+      luaLib = false;
+      initLua = false;
+      nvimRuntime = false;
+    };
+  };
 }
