ffa34f94f47fd59fde8c0916d1b3e1a72f5dcb4f
saygo-png <saygo.mail@proton.me>
2025-09-03 12:43:11 +0200

    telescope bug
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config.nix
+++ config.nix
@@ -1,2 +1,26 @@
 _: {
+  plugins = {
+    web-devicons.enable = true; # disable warn
+
+    lz-n.enable = true;
+    telescope.enable = true;
+    conform-nvim = {
+      enable = true;
+      lazyLoad.settings = {
+        keys = ["gq"];
+      };
+    };
+  };
+
+  performance = {
+    byteCompileLua = {
+      enable = true;
+      plugins = true;
+
+      configs = false;
+      luaLib = false;
+      initLua = false;
+      nvimRuntime = false;
+    };
+  };
 }
