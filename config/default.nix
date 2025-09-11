9b97e3874652b17f8cd029851d5309f72c8d6934
saygo-png <saygo.mail@proton.me>
2025-09-11 19:44:13 +0200

    feat: modularize autocomplete
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/default.nix
+++ config/default.nix
@@ -22,6 +22,7 @@ _: {
     ./plugins/ide/lsp.nix
     ./plugins/ide/linting.nix
     ./plugins/ide/formatting.nix
+    ./plugins/ide/autocomplete.nix
   ];
 
   opts = {
@@ -208,9 +209,6 @@ _: {
       vim.keymap.set('n', 'g*', 'g*zz', {silent = true, desc = "Search partial word under cursor and center" })
       vim.keymap.set('n', 'g#', 'g#zz', {silent = true, desc = "Search partial word under cursor (reverse) and center" })
 
-      -- Autocomplete
-      vim.keymap.set("i", "<C-x>", "<C-x><C-o>", { desc = "Autocomplete" })
-
       vim.keymap.set('n', '<leader>q', vim.cmd.only, { desc = "Quit other windows"})
       -- }}}
 
@@ -240,12 +238,6 @@ _: {
         symbols = ["─" "│" "┌" "┐" "└" "┘"];
       };
       lazyLoad.settings.event = "WinLeave";
-    };
-
-    lspkind = {
-      enable = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
-      cmp.enable = true;
     };
 
     project-nvim = {
@@ -309,53 +301,6 @@ _: {
 
           (nRegister "<leader>gs" "Gitsigns" " ")
         ];
-      };
-    };
-
-    luasnip = {
-      enable = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
-      settings = {
-        enable_autosnippets = true;
-        store_selection_keys = "<Tab>";
-      };
-    };
-
-    cmp = {
-      enable = true;
-      autoEnableSources = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
-      settings = {
-        autocomplete = true;
-        sources = [{name = "nvim_lsp";}];
-        performance = {
-          debounce = 200;
-          throttle = 200;
-          maxViewEntries = 5;
-          fetchingTimeout = 100;
-        };
-        snippet.expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
-        mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ select = false })";
-          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-        };
-
-        window = {
-          completion.scrollbar = true;
-          documentation.border = "single";
-        };
       };
     };
   };
---
+++ config/plugins/ide/autocomplete.nix
@@ -0,0 +1,64 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  extraConfigLua = ''
+    vim.keymap.set("i", "<C-x>", "<C-x><C-o>", { desc = "Autocomplete" })
+  '';
+
+  plugins = {
+    lspkind = {
+      enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+      cmp.enable = true;
+    };
+
+    luasnip = {
+      enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+      settings = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+      };
+    };
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+      settings = {
+        autocomplete = true;
+        sources = [{name = "nvim_lsp";}];
+        performance = {
+          debounce = 200;
+          throttle = 200;
+          maxViewEntries = 5;
+          fetchingTimeout = 100;
+        };
+        snippet.expand = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
+        mapping = {
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
+          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        };
+
+        window = {
+          completion.scrollbar = true;
+          documentation.border = "single";
+        };
+      };
+    };
+  };
+}
