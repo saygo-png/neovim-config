9e40b90192e19542fe3db3dd217898ad41d78f28
saygo-png <saygo.mail@proton.me>
2025-09-15 09:00:06 +0200

    refactor: default and keymaps
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/lib/keymaps.nix
+++ config/lib/keymaps.nix
@@ -1,3 +1,4 @@
+# This is the ugly part of the config. I make this ugly so the rest is pretty :).
 {
   config,
   lib,
@@ -12,31 +13,43 @@
     };
 
     kns = lib.mkOption {
-      default = action: desc: {
-        inherit action desc;
-        silent = false;
-      };
       readOnly = true;
       type = lib.types.anything;
       description = "Make keybind not silent";
+      default = action: desc: {inherit action desc;} // {silent = false;};
     };
 
-    my.keymaps = let
-      mkKeymapOption = mn: {
-        ${mn} = lib.mkOption {
-          default = {};
-          description = ''Set keybindings for ${mn} mode'';
-          type = with lib.types; attrsOf (attrsOf (either str bool));
-        };
+    wk = lib.mkOption {
+      readOnly = true;
+      description = "Make which-key registration";
+      default = group: icon: {inherit icon group;};
+    };
+
+    my = {
+      which-keys = lib.mkOption {
+        default = {};
+        description = "Registrations for which-key";
+        type = with lib.types; attrsOf (attrsOf str);
       };
-    in
-      lib.mergeAttrsList (map mkKeymapOption [
-        "normal"
-        "visual"
-      ]);
+      keymaps = let
+        mkKeymapOption = mn: {
+          ${mn} = lib.mkOption {
+            default = {};
+            description = "Set keybindings for ${mn} mode";
+            type = with lib.types; attrsOf (attrsOf (oneOf [str bool (attrsOf str)]));
+          };
+        };
+      in
+        lib.mergeAttrsList (map mkKeymapOption [
+          "normal"
+          "visual"
+          "insertAndCommand"
+        ]);
+    };
   };
 
   config = {
+    plugins.which-key.settings.spec = lib.mapAttrsToList (key: v: {__unkeyed = key;} // v) config.my.which-keys;
     keymaps = let
       toKeymapList = mode:
         lib.mapAttrsToList (
@@ -54,12 +67,13 @@
         );
 
       sharedOpts = {options.silent = true;};
-      inherit (config.my.keymaps) normal visual;
+      inherit (config.my.keymaps) normal visual insertAndCommand;
     in
       lib.nixvim.keymaps.mkKeymaps sharedOpts
       (lib.flatten [
         (toKeymapList "n" normal)
         (toKeymapList "v" visual)
+        (toKeymapList ["i" "c"] insertAndCommand)
       ]);
   };
 }
