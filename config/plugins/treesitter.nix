f841b6a7a91cdc0f9cc2484e3614212edcde6de1
saygo-png <saygo.mail@proton.me>
2025-09-20 15:34:24 +0200

    refactor: treesitter
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -3,52 +3,45 @@
   pkgs,
   ...
 }: {
+  opts.foldexpr = "nvim_treesitter#foldexpr()";
   performance.combinePlugins.standalonePlugins = ["nvim-treesitter"];
-
-  # opts.foldexpr = "nvim_treesitter#foldexpr()";
 
   # https://github.com/nvim-treesitter/nvim-treesitter/issues/7967
   extraFiles."ftplugin/haskell.vim".text = "set nocursorline";
 
   autoGroups."_cmd_win".clear = true;
-  autoCmd = [
-    {
-      group = "_cmd_win";
-      event = ["CmdWinEnter"];
-      callback.__raw = ''
-        function()
-          local ok, _ = pcall(vim.keymap.del, "n", "<CR>", { buffer = true })
-          if not ok then
-            -- Silently ignore error when node increment isnt set, like in q/
-          end
-        end
-      '';
-    }
-  ];
+  autoCmd = lib.singleton {
+    group = "_cmd_win";
+    event = ["CmdWinEnter"];
+    callback.__raw = ''
+      function()
+        local ok, _ = pcall(vim.keymap.del, "n", "<CR>", { buffer = true })
+        -- Silently ignore error when node increment isn't set like in q/
+        if not ok then end
+      end
+    '';
+  };
 
   plugins.treesitter = let
-    boolMatch = let
-      checkPassed =
-        lib.assertMsg
-        (builtins.match "abc" "abc" == [] && builtins.match "foo" "abc" == null)
-        "builtins.match must have changed";
-    in
-      regex: str: (builtins.match regex str) != null && checkPassed;
-
     inherit (pkgs.vimPlugins.nvim-treesitter.passthru) allGrammars;
+    boolMatch = regex: str: (builtins.match regex str) != null && checkPassed;
+    checkPassed =
+      lib.assertMsg
+      (builtins.match "abc" "abc" == [] && builtins.match "foo" "abc" == null)
+      "builtins.match must have changed";
     matchCommentGrammar = str: boolMatch ".*comment-grammar.*" str;
     filteredGrammars = builtins.filter (set: !matchCommentGrammar set.name) allGrammars;
   in {
     enable = true;
     folding = true;
     nixvimInjections = true;
-    nixGrammars = true; # Install grammars with Nix
     grammarPackages = filteredGrammars;
+    nixGrammars = true; # Install grammars with Nix
     settings = {
-      indent.enable = true;
-      ignore_install = ["comment"]; # Comment parser is very slow
       auto_install = false;
+      indent.enable = true;
       highlight.enable = true;
+      ignore_install = ["comment"]; # Comment parser is very slow
       incremental_selection = {
         enable = true;
         keymaps = {
