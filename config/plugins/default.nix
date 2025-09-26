d9f76a88d88a0bb552e9e0b48044e44a582a1386
saygo-png <saygo.mail@proton.me>
2025-09-27 00:46:26 +0200

    feat: add quickfix plugins
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,5 +1,10 @@
-_: {
+{pkgs, ...}: {
   # Plugins without any config beyond lazyloading go here.
+
+  extraPlugins = [
+    pkgs.vimPlugins.vim-qf
+  ];
+
   plugins = {
     direnv.enable = true;
     fugitive.enable = true;
@@ -10,6 +15,18 @@ _: {
     lzn-auto-require.enable = true;
 
     comment = {
+      enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+    };
+
+    nvim-bqf = {
+      enable = true;
+      extraOptions = {
+        preview.winblend = 0;
+      };
+    };
+
+    quicker = {
       enable = true;
       lazyLoad.settings.event = "DeferredUIEnter";
     };
