0156a61747e999735c8cd40f2a67e2713fc2bf75
saygo-png <saygo.mail@proton.me>
2025-10-05 12:48:11 +0200

    feat: add my linter
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/plugins/ide/linting.nix
+++ config/plugins/ide/linting.nix
@@ -10,11 +10,46 @@
     lint = {
       enable = true;
       lazyLoad.settings.event = "DeferredUIEnter";
+      customLinters = {
+        nlint = {
+          cmd = "nlint";
+          stdin = false;
+          parser.__raw = ''
+            function(output)
+              local severities = {
+                error = vim.diagnostic.severity.ERROR,
+                warning = vim.diagnostic.severity.WARN,
+                hint = vim.diagnostic.severity.HINT,
+              }
+              local diagnostics = {}
+              local items = #output > 0 and vim.json.decode(output) or {}
+              for _, item in ipairs(items) do
+                table.insert(diagnostics, {
+                  lnum = item.startLine - 1,
+                  col = item.startColumn - 1,
+                  end_lnum = item.endLine - 1,
+                  end_col = item.endColumn - 1,
+                  severity = severities[item.severity:lower()],
+                  source = "nlint",
+                  message = item.hint
+                            .. "\n"
+                            .. (item.from ~= vim.NIL and ("From: " .. item.from) or "")
+                            .. "\n"
+                            .. (item.to ~= vim.NIL and ("To: " .. item.to) or "")
+                            .. "\n" .. "\n"
+                            .. item.why,
+                })
+              end
+              return diagnostics
+            end
+          '';
+        };
+      };
       lintersByFt = {
         json = ["jsonlint"];
         bash = ["shellcheck"];
         shell = ["shellcheck"];
-        nix = ["nix" "deadnix" "statix"];
+        nix = ["nlint" "nix" "deadnix" "statix"];
       };
     };
   };
