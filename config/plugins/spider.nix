6dcb3e75030f6ce3eae7fc37394c8e3fcd28f178
saygo-png <saygo.mail@proton.me>
2025-11-13 22:16:58 +0100

    fix: spider lazyloading
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/plugins/spider.nix
+++ config/plugins/spider.nix
@@ -1,16 +1,23 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  inherit (config) k;
+  inherit (lib.nixvim) mkRaw;
+in {
   plugins.spider = {
     enable = true;
-    lazyLoad.settings.keys = ["b" "e" "ge" "w"];
+    lazyLoad.settings.keys = map (b: b // {mode = ["n" "o" "x"];}) (config.toLazyKeys
+      {
+        "b" = k (mkRaw "function() require('spider').motion('b') end") "Spider b";
+        "e" = k (mkRaw "function() require('spider').motion('e') end") "Spider e";
+        "ge" = k (mkRaw "function() require('spider').motion('ge') end") "Spider ge";
+        "w" = k (mkRaw "function() require('spider').motion('w') end") "Spider w";
+      });
     settings = {
       consistentOperatorPending = true;
       skipInsignificantPunctuation = false;
-    };
-    keymaps.motions = {
-      b = "b";
-      e = "e";
-      ge = "ge";
-      w = "w";
     };
   };
 }
