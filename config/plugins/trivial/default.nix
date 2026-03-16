c9660c291babdd48454924bcfee636ffbee70636
saygo-png <saygo.mail@proton.me>
2026-03-16 12:10:17 +0100

    feat: remove direnv.vim
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- config/plugins/trivial/default.nix
+++ config/plugins/trivial/default.nix
@@ -1,7 +1,6 @@
 {
   # Plugins without any config beyond lazyloading go here.
   plugins = {
-    direnv.enable = true;
     # fugitive.enable = true;
     # persistence.enable = true;
     vim-surround.enable = true;
